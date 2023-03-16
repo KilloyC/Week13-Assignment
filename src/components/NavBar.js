c732e2f16e61b029ec7cb8563e0dda26484a6eaf
Christopher Killoy <chrkilloy@gmail.com>
2023-03-16 09:56:55 -0600

    second commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ function NavBar() {
         return(
             <div className="NavBar">
                <ul>
-                    <li className="logo active">LOGO</li>
+                    <li className="logo active">DinoTrivia</li>
                     <li><a href="#login">Login</a></li>
                     <li><a href="#home">Home</a></li>
                     <li><a href="#about">About</a></li>
